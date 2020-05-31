1991316180f1e6a24f8a411bf714c3eafff4b5b0
LeonelGzz <leosnoob@outlook.com>
2020-05-31 00:41:33 -0500

    loading

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ var conteo = 1;
 var interval = setInterval('contar()',1000);
 var boton = document.getElementById('boton');
 
+
 function contar() {
-   boton.innerHTML=conteo;
+   boton.innerHTML='loading'+'.'.repeat(conteo);
    conteo = conteo + 1;
    if (conteo >=4) {
       conteo=1;

2fa14c10f7f23a10aa46b51fc09af36ec0e9f036
LeonelGzz <leosnoob@outlook.com>
2020-05-31 11:02:04 -0500

    pokeapi running

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
-var conteo = 1;
-var interval = setInterval('contar()',1000);
+var pokeId = 1;
+var interval = setInterval('display()', 1000);
 var boton = document.getElementById('boton');
 
 
-function contar() {
-   boton.innerHTML='loading'+'.'.repeat(conteo);
-   conteo = conteo + 1;
-   if (conteo >=4) {
-      conteo=1;
-   }
+function display() {
+   boton.innerHTML = pokeId.toString();
+   consultarPkmn()
 
+   pokeId++;
+   if (pokeId > 150) {
+      pokeId = 1;
+   }
+}
+
+function consultarPkmn() {
+   let pokeApi = "https://pokeapi.co/api/v2/pokemon/";
+   fetch(pokeApi + pokeId.toString())
+      .then(response => response.json())
+      .then(data => console.log(data.name))
 }

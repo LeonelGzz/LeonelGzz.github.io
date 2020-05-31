be871d405732d2dc38ae5c17967da9cb01baef78
LeonelGzz <leosnoob@outlook.com>
2020-05-31 00:34:35 -0500

    conteo

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-console.log(" :) ");
-console.log(" bs is on ");
-alert("bootstrap is running");
+var conteo = 1;
+var interval = setInterval('contar()',1000);
+var boton = document.getElementById('boton');
+
+function contar() {
+   boton.innerHTML=conteo;
+   conteo = conteo + 1;
+   if (conteo >=4) {
+      conteo=1;
+   }
+
+}
